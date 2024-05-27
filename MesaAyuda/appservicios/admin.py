d644d8fdeb7df9d0b0c93151a3432001da6d536a
juls2404 <juliethximenarendonsanchez@gmail.com>
2024-05-27 08:13:26 -0500

    AppMesaServicios

---
+++ MesaAyuda/appservicios/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from appservicios.models import OficinaAmbiente, User, TipoProcedimiento
+
+# Register your models here.
+
+admin.site.register(OficinaAmbiente)
+admin.site.register(User)
+admin.site.register(TipoProcedimiento)
+
---
+++ MesaAyuda/appservicios/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib import auth
+from appservicios.models import *
+from django.db import Error, transaction
+from random import *
+
+# Create your views here.
+def inicio(request):
+    return render (request, "formulario.html")
+
+
+def login (request):
+    username = request.POST ["User"]
+    password = request.POST ["Password"]
+    user = authenticate(username = username, password = password)
+    
+    if user is not None:
+    #Aquí se registra la variable de sesión 
+    
+        if user.groups.filtrer(name = 'administrador'). exists():
+            return redirect ('inicioAdministrador')
+    
+        elif user.groups.filter(name='Tecnico'). exists():
+            return redirect ('inicioTecnico')
+    
+        else: return redirect ('inicioEmpleado')
+    else:
+        mensaje = "Tu usuario o contraseña son incorrectos"
+        return render (request, "formulario.html",{"mensaje": mensaje})
+    
+
+def vistaSolicitud(request):
+    if request.user.is_authenticated:
+        oficinaAmbientes = OficinaAmbiente.objects.all()
+        datosSesion = { "user": request.user,
+                        "rol": request.user.groups.get().name,
+                        'oficinaAmbiente': oficinaAmbientes}
+        mensaje = "Debe iniciar sesión"
+        return render(request, 'empleado/solicitud.html',{"mensaje": mensaje})
+
+def registroSolicitud(request):
+    
+    try:
+        with transaction.atomic():
+            user = request.user
+            descripcion = request.POST ['descripción']
+            idOfAmb = request. POST ['idOfAmb']
+            oficinaAmbiente = OficinaAmbiente.objects.get(pk=idOfAmb)
+            solicitud = Solicitud (solUsuario = user,
+                                    solDescripción = descripcion,
+                                    solOficinaAmbiente = oficinaAmbiente)
+            solicitud.save()
+            consecutivoCaso= randint(1,10000)
+            codigoCaso = "REQ" + str (consecutivoCaso). rjust (5,'0')
+            userCaso = user.objects.filter(groups__name__in=['Administrador'])
+            estado = "Solicitada"
+            caso = Caso (casSolicitud = solicitud,
+                            casCodigo = codigoCaso,
+                            casUsuario = userCaso,
+                            casEstado =  estado
+                            )
+            caso.save
+    except Error as error:
+        transaction.rollback()
+        mensaje = f"{error}"
+        
+        
+
+
+def Salir(request):
+    auth.logout(request)
+    return render(request, "formulario.html", {"mensaje": "Ha cerrado la sesión"})
